f9aa321ef6fd7a8a1a4c3cef42c055746ef7fef0
dudhit <dudhit@users.noreply.github.com>
2017-03-28 13:35:22 +1030

    add internal windows lookup functionality

---
+++ CircleBluePrint/FileSystemAdministrativeTools.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
+{
+  public class FileSystemAdministrativeTools
+  {
+    public string SteamPath { get; set; }
+    public string SteamUserName { get; set; }
+    public string SteamUserId { get; set; }
+    private string compareUser;
+    private bool getSteamId;
+    public FileSystemAdministrativeTools(string user) : this(user, true) { }
+
+    public FileSystemAdministrativeTools() : this(null) { }
+    public FileSystemAdministrativeTools(string user, bool getId)
+    {
+      this.compareUser=user;
+      this.getSteamId=getId;
+      GetSteamData();
+    }
+
+
+    public string GetGameDataSaveLocation()
+    {
+      string userApp = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+      return userApp + "\\SpaceEngineers\\Blueprints\\local";
+
+
+    }
+    public string ManuallySelectSaveFolder()
+    {
+      string filePath=string.Empty;
+      Microsoft.Win32.OpenFileDialog findFolder = new Microsoft.Win32.OpenFileDialog();
+      findFolder.Filter = "All files (*.*)|*.*";
+      if(findFolder.ShowDialog() == true)
+      {
+        //split path into individual folders
+        char[] delimiter = new char[] { '\\' };
+        int fileNameLength = findFolder.FileName.Length;
+        string[] individualFolders = findFolder.FileName.Split(delimiter);
+        //rejoin excluding the file selected
+        for(int i = 0;i < individualFolders.Length - 1;i++)
+        {
+          filePath += individualFolders[i] + "\\";
+        }
+        findFolder = null;
+      }
+      return filePath;
+    }
+
+    private bool RegistryHasSteamData()
+    {
+      RegistryKey regKey = Registry.CurrentUser;
+      regKey = regKey.OpenSubKey(@"Software\Valve\Steam");
+
+      if(regKey != null)
+      {
+        SteamPath = regKey.GetValue("SteamPath").ToString();
+        SteamUserName = regKey.GetValue("LastGameNameUsed").ToString();
+        if(string.IsNullOrEmpty(compareUser))
+          compareUser=SteamUserName;
+        return true;
+      }
+      return false;
+    }
+
+    private void GetSteamData()
+    {
+      if(RegistryHasSteamData())
+      {
+        if(getSteamId)
+        {
+          if(!(compareUser.Equals(SteamUserName)))
+          { SteamUserName=compareUser; }
+          using(SoloProjects.Dudhit.SpaceEngineers.SEBP.View.FindSteamID fsID =
+           new SoloProjects.Dudhit.SpaceEngineers.SEBP.View.FindSteamID(SteamPath, SteamUserName)) { SteamUserId = fsID.SteamID; }
+        }
+      }
+      else
+      {
+        MessageBox.Show("Windows could not return the location of your Steam installation\n manual entry required", "Unknown Steam Installation location", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+      }
+    }
+  }
+}

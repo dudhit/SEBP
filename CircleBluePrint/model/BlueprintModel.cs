22689dd1a8a4d5a1d3eb0e9404c1bc71bbb1f362
dudhit <dudhit@users.noreply.github.com>
2017-03-28 13:44:17 +1030

    Enum isertion and setter validations

--- CircleBluePrint/model/BlueprintModel.cs
+++ CircleBluePrint/model/BlueprintModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using SoloProjects.Dudhit.Utilites;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
 {
   public class BlueprintModel : INotifyPropertyChanged, IDataErrorInfo
   {
+    [Flags]private enum ShapeCombinations {nil=0,circle=1<<0,ellipse=1<<1,sphere=1<<2,ellipsiod=1<<3,quarter=1<<4,semi=1<<5,full=1<<6 }
+    [Flags]
+    private enum ModelState
+    { nil=0, hasBlueprintFilePath=1<<0, hasSteamId=1<<1, hasSteamName=1<<2, hasBlueprintName=1<<3, hasXAxis=1<<4, hasYAxis=1<<5, hasZAxis=1<<6, hasShape=1<<7, hasFraction=1<<8, hasBlockArmour=1<<9, hasBlockSize=1<<10, hasBlockColour=1<<11, all= hasBlueprintFilePath|hasSteamId|hasSteamName|hasBlueprintName|hasXAxis| hasYAxis| hasZAxis| hasShape| hasFraction|hasBlockArmour| hasBlockSize| hasBlockColour }
     private string blueprintFilePath;
     private string steamId;
     private string steamName;
@@ -13,12 +19,17 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
     private int yAxis;
     private int zAxis;
     private string shape;
+    private string shapeFraction;
+    private int finalShape;
     private string blockArmour;
     private string blockSize;
     private SeHSV blockColour;
-    private byte modelState;
+    private int modelState;
+
     public BlueprintModel()
-    { modelState=0; }
+    {
+      modelState=0;
+    }
     #region properties
 
 
@@ -27,24 +38,27 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.blueprintFilePath; }
       set
       {
+        ResetSetFlag( (int)ModelState.hasBlueprintFilePath);
         if(NoEmptyString(value))
         {
           this.blueprintFilePath = value;
-          this.modelState+=1;
+          this.modelState+=(int)ModelState.hasBlueprintFilePath;
           RaisePropertyChanged("BlueprintFilePath");
         }
       }
     }
+
 
     public string SteamId
     {
       get { return this.steamId; }
       set
       {
+        ResetSetFlag((int)ModelState.hasSteamId);
         if(NoEmptyString(value))
         {
-          this.steamId = value;
-          modelState+=2;
+          this.steamId = value.Trim();
+          modelState+=(int)ModelState.hasSteamId;
           RaisePropertyChanged("SteamId");
         }
       }
@@ -54,11 +68,13 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.steamName; }
       set
       {
+        ResetSetFlag((int)ModelState.hasSteamName);
         if(NoEmptyString(value))
         {
-          this.steamName = value;
-          this.modelState+=1;
-        RaisePropertyChanged("SteamName");}
+          this.steamName = value.Trim();
+          this.modelState+=(int)ModelState.hasSteamName;
+          RaisePropertyChanged("SteamName");
+        }
       }
     }
     public string BlueprintName
@@ -66,24 +82,29 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.blueprintName; }
       set
       {
+        ResetSetFlag((int)ModelState.hasBlueprintName);
         if(NoEmptyString(value))
         {
-          this.blueprintName = value;
-          this.modelState+=1;
-        
-        RaisePropertyChanged("BlueprintName");
-      }}
+          this.blueprintName = value.Trim();
+          this.modelState+=(int)ModelState.hasBlueprintName;
+          RaisePropertyChanged("BlueprintName");
+        }
+      }
     }
     public int XAxis
     {
       get { return this.xAxis; }
       set
       {
+        ResetSetFlag((int)ModelState.hasXAxis);
         if(ValidDouble(value))
         {
-          this.xAxis = value;
-          this.modelState+=1;
-          RaisePropertyChanged("XAxis");
+          if(value>=10||value<=500)
+          {
+            this.xAxis = value;
+            this.modelState+=(int)ModelState.hasXAxis;
+            RaisePropertyChanged("XAxis");
+          }
         }
       }
     }
@@ -92,11 +113,15 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.yAxis; }
       set
       {
+        ResetSetFlag((int)ModelState.hasYAxis);
         if(ValidDouble(value))
         {
-          this.yAxis = value;
-          this.modelState+=1;
-          RaisePropertyChanged("YAxis");
+          if(value>=10||value<=500)
+          {
+            this.yAxis = value;
+            this.modelState+=(int)ModelState.hasYAxis;
+            RaisePropertyChanged("YAxis");
+          }
         }
       }
     }
@@ -105,38 +130,84 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.zAxis; }
       set
       {
+        ResetSetFlag((int)ModelState.hasZAxis);
         if(ValidDouble(value))
         {
-          this.zAxis = value;
-          this.modelState+=1;
-          RaisePropertyChanged("ZAxis");
+          if(value>=10||value<=500)
+          {
+            this.zAxis = value;
+            this.modelState+=(int)ModelState.hasZAxis;
+            RaisePropertyChanged("ZAxis");
+          }
         }
       }
     }
     public string Shape
     {
-      get { return this.shape; }
+      get { return Enum.Parse(typeof(ShapeCombinations), this.shape).ToString(); }
       set
       {
-        if(NoEmptyString(value))
+        ResetSetFlag((int)ModelState.hasShape);
+        if(Enum.IsDefined(typeof(ShapeCombinations), value))
         {
-          this.shape = value;
-          this.modelState+=1;
+          this.shape =  Enum.Parse(typeof(ShapeCombinations), value).ToString();
+          this.modelState+=(int)ModelState.hasShape;
+          RaisePropertyChanged("Shape");
+          RaisePropertyChanged("FinalShape");
         }
-        RaisePropertyChanged("Shape");
       }
+    }
+    public string ShapeFraction
+    {
+      get { return Enum.Parse(typeof(ShapeCombinations), this.shapeFraction).ToString(); }
+      set
+      {
+     
+        ResetSetFlag((int)ModelState.hasFraction);
+        if(Enum.IsDefined(typeof(ShapeCombinations), value))
+        {
+          this.shapeFraction =Enum.Parse(typeof(ShapeCombinations), value).ToString();
+          this.modelState+=(int)ModelState.hasFraction;
+          RaisePropertyChanged("ShapeFraction");
+          RaisePropertyChanged("FinalShape");
+        }
+      }
+    }
+    public int FinalShape
+    { 
+      get 
+    {
+      return this.finalShape;
+    } 
+      set
+    {
+        int join=(int)ModelState.hasShape|(int)ModelState.hasFraction;
+      if(IsFlagSet(this.modelState,join)==join)
+      {
+        JoinShapeCombinationsEnumStings(this.shape, this.shapeFraction);
+    }
+    } 
+    }
+
+    private int JoinShapeCombinationsEnumStings(  string value1,string value2)
+    {
+      return this.finalShape=(int)Enum.Parse(typeof(ShapeCombinations), value1)+(int)Enum.Parse(typeof(ShapeCombinations), value2);
     }
     public string BlockArmour
     {
       get { return this.blockArmour; }
       set
       {
+        ResetSetFlag((int)ModelState.hasBlockArmour);
         if(NoEmptyString(value))
         {
-          this.blockArmour = value;
-          this.modelState+=1;
+          if(value.Equals("Normal")||value.Equals("Heavy"))
+          {
+            this.blockArmour = value;
+            this.modelState+=(int)ModelState.hasBlockArmour;
+            RaisePropertyChanged("BloakArmour");
+          }
         }
-        RaisePropertyChanged("BloakArmour");
       }
     }
     public string BlockSize
@@ -144,12 +215,16 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.blockSize; }
       set
       {
+        ResetSetFlag((int)ModelState.hasBlockSize);
         if(NoEmptyString(value))
         {
-          this.blockSize = value;
-          this.modelState+=1;
+          if(value.Equals("Large")||value.Equals("Small"))
+          {
+            this.blockSize = value;
+            this.modelState+=(int)ModelState.hasBlockSize;
+            RaisePropertyChanged("BlockSize");
+          }
         }
-        RaisePropertyChanged("BlockSize");
       }
     }
     public SeHSV BlockColour
@@ -157,15 +232,27 @@ namespace SoloProjects.Dudhit.SpaceEngineers.SEBP
       get { return this.blockColour; }
       set
       {
+        ResetSetFlag((int)ModelState.hasBlockColour);
         this.blockColour = value;
-        this.modelState+=1;
+        this.modelState+=(int)ModelState.hasBlockColour;
         RaisePropertyChanged("BlockColour");
       }
     }
 
-    public bool IsComplete { get { return this.modelState==(byte)255; } }
+
+
+    public bool IsComplete { get { return this.modelState==(int)ModelState.all; } }
     #endregion
 
+    private void ResetSetFlag( int flag)
+    {
+      if(IsFlagSet(this.modelState, flag)==flag) { this.modelState-=flag; }
+    }
+
+    private int IsFlagSet(int flagTotal, int singleFlag)
+    {
+      return flagTotal&singleFlag;
+    }
     #region INotifyPropertyChanged Members
 
     public event PropertyChangedEventHandler PropertyChanged;

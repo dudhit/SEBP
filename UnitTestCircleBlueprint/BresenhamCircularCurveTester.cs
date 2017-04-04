b267c43c082c3de15457db9f4c74d54d786cf4de
dudhit <dudhit@users.noreply.github.com>
2017-04-04 10:09:36 +0930

    added curve testers

---
+++ UnitTestCircleBlueprint/BresenhamCircularCurveTester.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SoloProjects.Dudhit.Utilities;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Dudhit.UnitTestCircleBlueprint
+{
+  [TestFixture]
+  class BresenhamCircularCurveTester
+  {
+    private BresenhamCircularCurve curve;
+    private List<Point> data;
+    [SetUp]
+    public void init()
+    {
+      curve = new BresenhamCircularCurve(60);
+      data = new List<Point>();
+      System.Diagnostics.Trace.WriteLine("setep complete");
+    }
+
+    [Test]
+    public void Rungenerator()
+    { curve.BeginCalculations();
+    data=curve.GetCurve();
+    foreach(Point p in data)
+    { System.Diagnostics.Trace.WriteLine(p.ToString());
+    }
+    Assert.Greater(data.Count,1);
+    }
+
+    [TearDown]
+    public void Dispose()
+    { curve=null; }
+  }
+}
---
+++ UnitTestCircleBlueprint/BresenhamEllipticalCurveTester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using SoloProjects.Dudhit.Utilities;
+using System.Windows;
+using NUnit.Framework;
+
+namespace Dudhit.UnitTestCircleBlueprint
+{
+  [TestFixture]
+  class BresenhamEllipticalCurveTester
+  {
+    private BresenhamEllipticalCurve curve;
+    private List<Point> data;
+    [SetUp]
+    public void init()
+    {
+      curve = new BresenhamEllipticalCurve(20,10);
+      data = new List<Point>();
+      System.Diagnostics.Trace.WriteLine("setep complete");
+    }
+
+    [Test]
+    public void Rungenerator()
+    {
+      curve.BeginCalculations();
+      data=curve.GetCurve();
+      foreach(Point p in data)
+      {
+        System.Diagnostics.Trace.WriteLine(p.ToString());
+      }
+      Assert.Greater(data.Count, 1);
+    }
+
+    [TearDown]
+    public void Dispose()
+    { curve=null; }
+  }
+}

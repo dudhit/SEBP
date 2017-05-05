1232071d6ccc99fe6ed42b62676e640711f8133b
dudhit <dudhit@users.noreply.github.com>
2017-05-05 17:18:16 +0930

    TESTING!!!

--- UnitTestCircleBlueprint/03OuputTests/XmlTester.cs
+++ UnitTestCircleBlueprint/03OuputTests/XmlTester.cs
@@ -13,25 +13,74 @@ namespace SoloProjects.Dudhit.Utilities.UnitTestCircleBlueprint
   [TestFixture]
   public class XmlTester
   {
-  private  BlueprintModel myBLueprintModel;
- private   HashSet<Point3D> testData;
-    FileSystemAdministrativeTools fsat;
-
+    private BlueprintModel myBLueprintModel;
+    private HashSet<Point3D> testData;
+    private string testPath;
+    private string blueprintName;
+    private string blockSize;
+    private string blockArmour;
     [Test]
-    public void TestMethod1()
+    public void TestLargeLight()
     {
-      fsat = new FileSystemAdministrativeTools("dudhit");
+      testPath = Directory.GetCurrentDirectory();
+       blueprintName="test1";
+      Directory.CreateDirectory(Path.Combine(testPath, blueprintName));
+      blockSize="Large";
+      blockArmour="Normal";
+            RepeatingChunk();
+      FileAssert.Exists(Path.Combine(testPath, blueprintName, "bp.sbc"));
+      System.Diagnostics.Trace.WriteLine(string.Format("Outputs found at:",Path.Combine(testPath,blueprintName)));
+    }
+    [Test]
+    public void TestLargeHeavy()
+    {
+      testPath = Directory.GetCurrentDirectory();
+       blueprintName="test2";
+      Directory.CreateDirectory(Path.Combine(testPath, blueprintName));
+      blockSize="Large";
+      blockArmour="Heavy";
+      RepeatingChunk();
+      FileAssert.Exists(Path.Combine(testPath, blueprintName, "bp.sbc"));
+      System.Diagnostics.Trace.WriteLine(string.Format("Outputs found at:", Path.Combine(testPath, blueprintName)));
+    }
+    [Test]
+    public void TestSmallLight()
+    {
+      testPath = Directory.GetCurrentDirectory();
+       blueprintName="test3";
+      Directory.CreateDirectory(Path.Combine(testPath, blueprintName));
+      blockSize="Small";
+      blockArmour="Normal";
+      RepeatingChunk();
+      FileAssert.Exists(Path.Combine(testPath, blueprintName, "bp.sbc"));
+      System.Diagnostics.Trace.WriteLine(string.Format("Outputs found at:", Path.Combine(testPath, blueprintName)));
+    }
+    [Test]
+    public void TestSmallHeavy()
+    {
+      testPath = Directory.GetCurrentDirectory();
+       blueprintName="test4";
+      Directory.CreateDirectory(Path.Combine(testPath, blueprintName));
+      blockSize="Small";
+      blockArmour="Heavy";
+      RepeatingChunk();
+      FileAssert.Exists(Path.Combine(testPath, blueprintName, "bp.sbc"));
+      System.Diagnostics.Trace.WriteLine(string.Format("Outputs found at:", Path.Combine(testPath, blueprintName)));
+    }
+
+    private void RepeatingChunk()
+    {
       myBLueprintModel = new BlueprintModel();
-      myBLueprintModel.BlockSize="Large";
-      myBLueprintModel.BlockArmour="Normal";
-      myBLueprintModel.BlueprintName="testclassbp";
-      myBLueprintModel.BlueprintFilePath=fsat.GetGameDataSaveLocation();
+      myBLueprintModel.BlockSize=blockSize;
+      myBLueprintModel.BlockArmour=blockArmour;
+      myBLueprintModel.BlueprintName=blueprintName;
+      myBLueprintModel.BlueprintFilePath=testPath;
       myBLueprintModel.BlockColour=new Utilities.SeHSV(0, -100, -90);
       myBLueprintModel.Shape="circle";
       myBLueprintModel.ShapeFraction="quarter";
       myBLueprintModel.Solid=false;
-      myBLueprintModel.SteamId=fsat.SteamUserId;
-      myBLueprintModel.SteamName=fsat.SteamUserName;
+      myBLueprintModel.SteamId="123456789";
+      myBLueprintModel.SteamName="itsme";
       myBLueprintModel.XAxis=11;
       myBLueprintModel.YAxis=12;
       myBLueprintModel.ZAxis=13;
@@ -39,16 +88,14 @@ namespace SoloProjects.Dudhit.Utilities.UnitTestCircleBlueprint
       if(myBLueprintModel.HasUsableData)
       {
         testData =new HashSet<Point3D>();
-        testData.Add(new Point3D(0,0,0));
-        testData.Add(new Point3D(1,0,0));
-        testData.Add(new Point3D(2,0,0));
-        testData.Add(new Point3D(0,1,0));
+        testData.Add(new Point3D(0, 0, 0));
+        testData.Add(new Point3D(1, 0, 0));
+        testData.Add(new Point3D(2, 0, 0));
+        testData.Add(new Point3D(0, 1, 0));
         Assert.Greater(testData.Count, 0);
-                BluePrintXml mfh = new BluePrintXml(myBLueprintModel,testData);
-                mfh.MakeBaseStructure();        
+        BluePrintXml mfh = new BluePrintXml(myBLueprintModel, testData);
+        mfh.MakeBaseStructure();
         mfh.BluePrintFileHandling();
-        string fullpath = myBLueprintModel.BlueprintFilePath+"\\"+myBLueprintModel.BlueprintName;
-        Assert.IsTrue(Directory.Exists(fullpath));
       }
     }
   }

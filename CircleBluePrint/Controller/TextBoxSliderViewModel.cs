c0fbe91e622a31382e95fdd22da6ff31b5144f55
dudhit <dudhit@users.noreply.github.com>
2017-06-08 12:51:45 +0930

    new textbox slider combo View model

---
+++ CircleBluePrint/Controller/TextBoxSliderViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SoloProjects.Dudhit.SpaceEngineers.SEBP.model
+{
+  public class TextBoxSliderViewModel : BindingBase
+  {
+
+    public ObservableCollection<MyTextSliderModel> SliderCollection { get; set; }
+    public double NumberItems { get; set; }
+    public MyTextSliderModel mts1;
+    public MyTextSliderModel mts2;
+    public MyTextSliderModel mts3;
+    public MyTextSliderModel mts4;
+    public MyTextSliderModel mts5;
+    public MyTextSliderModel mts6;
+    public MyTextSliderModel mts7;
+    public MyTextSliderModel mts8;
+    private ObservableCollection<MyTextSliderModel> temp;
+    public TextBoxSliderViewModel()
+      : base()
+    {
+      temp = new ObservableCollection<MyTextSliderModel>();
+
+      mts1 = new MyTextSliderModel();
+      mts2 = new MyTextSliderModel();
+      mts3 = new MyTextSliderModel();
+      mts4 = new MyTextSliderModel();
+      mts5 = new MyTextSliderModel();
+      mts6 = new MyTextSliderModel();
+      mts7 = new MyTextSliderModel();
+      mts1.Identifier = "x";
+      mts1.TextValue="6";
+      mts1.SliderMin=10;
+      mts1.SliderMax=100;
+      mts1.SliderValue=6;
+      mts2.Identifier = "y";
+      mts2.TextValue="6";
+      mts2.SliderMin=10;
+      mts2.SliderMax=200;
+      mts2.SliderValue=6;
+      mts2.SliderHeight=200;
+      mts3.Identifier = "z";
+      mts3.TextValue="6";
+      mts3.SliderMin=10;
+      mts3.SliderMax=500;
+      mts3.SliderValue=6;
+      mts7.SliderValue=4;
+      temp.Add(mts1);
+      temp.Add(mts2);
+      temp.Add(mts3);
+      temp.Add(mts4);
+      temp.Add(mts5);
+      temp.Add(mts6);
+      temp.Add(mts7);
+      SliderCollection=temp;
+      NumberItems=4;
+      RaisePropertyChanged("SliderCollection");
+      RaisePropertyChanged("NumberItems");
+
+    }
+
+
+  }
+
+}
---
+++ CircleBluePrint/Model/TextBoxSliderModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SoloProjects.Dudhit.SpaceEngineers.SEBP.model
+{
+
+  public class MyTextSliderModel : BindingBase
+  {
+    private string identifier;
+    private string textValue;
+    private double sliderMin;
+    private double sliderMax;
+    private double sliderValue;
+    private double sliderHeight;
+
+    public MyTextSliderModel():base()
+    {
+      identifier="this slider";
+      textValue="5";
+      sliderMax=10;
+      sliderMin=1;
+      sliderValue=5;
+      sliderHeight=50;
+      RaisePropertyChanged("Identifier");
+      RaisePropertyChanged("TextValue");
+      RaisePropertyChanged("SliderMin");
+      RaisePropertyChanged("SliderMax");
+      RaisePropertyChanged("SliderValue");
+      RaisePropertyChanged("SliderHeight");
+    }
+
+    public string Identifier
+    {
+      get { return this.identifier; }
+      set
+      {
+        if(value!=string.Empty)
+        {
+          this.identifier=value;
+        }
+        else
+          Identifier=identifier;
+        RaisePropertyChanged("Identifier");
+      }
+    }
+    public string TextValue
+    {
+      get { return this.textValue; }
+      set
+      {
+        double converted;
+        if(Double.TryParse(value, out converted))
+        {
+          if(converted>=sliderMin&&converted<=sliderMax&&converted.ToString()!=textValue)
+          {
+            SliderValue=converted;
+            this.textValue= converted.ToString();
+          }
+        }
+        else
+          TextValue=textValue;
+        RaisePropertyChanged("TextValue");
+      }
+    }
+    public double SliderMin
+    {
+      get { return this.sliderMin; }
+      set
+      {
+        this.sliderMin= value;
+        RaisePropertyChanged("SliderMin");
+      }
+    }
+    public double SliderMax
+    {
+      get { return this.sliderMax; }
+      set
+      {
+        this.sliderMax= value;
+        RaisePropertyChanged("SliderMax");
+      }
+    }
+    public double SliderValue
+    {
+      get { return this.sliderValue; }
+      set
+      {
+        if(value!=sliderValue)
+        {
+          this.sliderValue=Math.Round(value);
+        RaisePropertyChanged("SliderValue");
+        textValue=sliderValue.ToString();
+        RaisePropertyChanged("TextValue");
+        }
+      }
+    }
+    public double SliderHeight
+    {
+      get { return this.sliderHeight; }
+      set
+      {
+        this.sliderHeight= value;
+        RaisePropertyChanged("SliderHeight");
+      }
+    }
+  }
+
+}

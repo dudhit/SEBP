980638ba93a7f251bb08e92623025557becc4044
dudhit <dudhit@users.noreply.github.com>
2017-06-08 12:52:20 +0930

    new radiobutton coloured rectangle combo

---
+++ CircleBluePrint/Controller/RadioWithColouredBoxViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SoloProjects.Dudhit.SpaceEngineers.SEBP.model
+{
+  public class RadioWithColouredBoxViewModel : BindingBase
+  {
+
+    public ObservableCollection<MyRadioRectangleModel> ControlCollection { get; set; }
+    public double NumberItems { get; set; }
+    public MyRadioRectangleModel rm1;
+    public MyRadioRectangleModel rm2;
+    public MyRadioRectangleModel rm3;
+    public MyRadioRectangleModel rm4;
+    public MyRadioRectangleModel rm5;
+    public MyRadioRectangleModel rm6;
+    public MyRadioRectangleModel rm7;
+    public MyRadioRectangleModel rm8;
+    public MyRadioRectangleModel rm9;
+    public MyRadioRectangleModel rm10;
+    public MyRadioRectangleModel rm11;
+    public MyRadioRectangleModel rm12;
+    public MyRadioRectangleModel rm13;
+    public MyRadioRectangleModel rm14;
+    public MyRadioRectangleModel rm15;
+    public MyRadioRectangleModel rm16;
+    public MyRadioRectangleModel rm17;
+    public MyRadioRectangleModel rm18;
+    public MyRadioRectangleModel rm19;
+    public MyRadioRectangleModel rm20;
+    public MyRadioRectangleModel rm21;
+    public MyRadioRectangleModel rm22;
+    public MyRadioRectangleModel rm23;
+    public MyRadioRectangleModel rm24;
+    public MyRadioRectangleModel rm25;
+    public MyRadioRectangleModel rm26;
+    public MyRadioRectangleModel rm27;
+    public MyRadioRectangleModel rm28;
+    public MyRadioRectangleModel rmCustom;
+
+    private ObservableCollection<MyRadioRectangleModel> temp;
+    public RadioWithColouredBoxViewModel()
+      : base()
+    {
+      temp= new ObservableCollection<MyRadioRectangleModel>();
+
+      NumberItems=7;
+      ControlCollection=temp;
+      RaisePropertyChanged("NumberItems");
+      RaisePropertyChanged("ControlCollection");
+    }
+
+    public void ChangeDisplayColumns(int column)
+    {
+      if(column>0)
+      {
+        NumberItems=column;
+        RaisePropertyChanged("NumberItems");
+      }
+    }
+
+    public void AddModel(string group, bool isChecked, double width, double height, string colour)
+    {
+      temp=ControlCollection;
+      temp.Add(new MyRadioRectangleModel() { RadioGroup=group, RadioChecked=isChecked, RadioWidth=width, RadioHeight=height, RectFill=colour });
+      ControlCollection=temp;
+      RaisePropertyChanged("ControlCollection");
+    }
+
+    public void RemoveModel(string colour)
+    {
+      temp= ControlCollection;
+      foreach(MyRadioRectangleModel rrm in temp)
+      {
+        if(rrm.RectFill.Equals(colour))
+        {
+          temp.Remove(rrm);
+        }
+      }
+      ControlCollection=temp;
+      RaisePropertyChanged("ControlCollection");
+    }
+
+
+  }
+}
---
+++ CircleBluePrint/Model/MyRadioRectangleModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SoloProjects.Dudhit.SpaceEngineers.SEBP.model
+{
+  public class MyRadioRectangleModel : BindingBase
+  {
+    private string radioGroup;
+    private string radioTips;
+    private bool radioChecked;
+    private double radioWidth;
+    private double radioHeight;
+    private string rectFill;
+  
+
+    public MyRadioRectangleModel():base()
+    {
+    RadioGroup="mygrp";
+    RadioChecked=false;
+    RadioWidth=40;
+    RadioHeight=40;
+    RectFill="#FF808080";
+    }
+
+    public string RadioGroup
+    {
+      get { return this.radioGroup; }
+      set
+      {
+        if(value!=radioGroup&&value!=string.Empty)
+        {
+          this.radioGroup=value;
+          RaisePropertyChanged("RadioGroup");
+        }
+      }
+    }
+    public string RadioTips
+    {
+      get { return this.radioTips; }
+      set
+      {
+        if(value!=radioTips&&value!=string.Empty)
+        {
+          this.radioTips=value;
+          RaisePropertyChanged("RadioTips");
+        }
+      }
+    }
+    public bool RadioChecked
+    {
+      get { return this.radioChecked; }
+      set
+      {
+        if(value!=radioChecked)
+        {
+          this.radioChecked=value;
+          RaisePropertyChanged("RadioChecked");
+        }
+      }
+    }
+    public double RadioWidth
+    {
+      get { return this.radioWidth; }
+      set { if(value!=radioWidth&&value>13) { this.radioWidth=value; RaisePropertyChanged("RadioWidth"); RectWidth=value-14; RaisePropertyChanged("RectWidth"); } }
+    }
+    public double RadioHeight
+    {
+      get { return this.radioHeight; }
+      set { if(value!=radioHeight&&value>13) { this.radioHeight=value; RaisePropertyChanged("RadioHeight"); RectHeight=value-14; RaisePropertyChanged("RectHeight"); } }
+    }
+    public double RectWidth
+    {
+      get;
+     private set ; 
+    }
+    public double RectHeight
+    {
+      get ;
+      private set; 
+    }
+    public string RectFill
+    {
+      get { return this.rectFill; }
+      set
+      {
+        if(value!=rectFill)
+        {
+          this.rectFill=value;
+          RaisePropertyChanged("RectFill");
+          RadioTips=RectFill;
+        }
+      }
+    }
+  }
+}
